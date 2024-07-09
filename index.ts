9d758e9b2fd31cc6595bfd07abb5a5066a1fa602
Mirfan326 <ihmuhammadirfan@gmail.com>
2024-07-09 18:20:29 +0500

    task 2 completed

--- index.ts
+++ index.ts
@@ -2,6 +2,23 @@
 // tsc --init
 // tsc
 
+// …or create a new repository on the command line
+
+// echo "# git-and-git-hub-learning-1" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Mirfan326/git-and-git-hub-learning-1.git
+// git push -u origin main
+
+
+// …or push an existing repository from the command line
+
+// git remote add origin https://github.com/Mirfan326/git-and-git-hub-learning-1.git
+// git branch -M main
+// git push -u origin main
+
 // 1step: npm init -y
 // 2step: git add .
 // 3step: git commit -m "package.json"
@@ -14,8 +31,10 @@
 // Then create a file index.ts then adopt above steps:
 // git add . 
 // git commit -m "index.ts file added"
+// git remote add origin https://github.com/Mirfan326/git-and-git-hub-learning-1.git
+// git push -u origin master   
 
 
-
+console.log("Hello World!");
 
 console.log("Hello World!");
