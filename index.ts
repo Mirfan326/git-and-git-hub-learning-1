db1f198cc5483855849c57c13982a75e423e7178
Mirfan326 <ihmuhammadirfan@gmail.com>
2024-07-09 18:05:29 +0500

    index.ts file added

---
+++ index.ts
@@ -0,0 +1,21 @@
+// npm init -y
+// tsc --init
+// tsc
+
+// 1step: npm init -y
+// 2step: git add .
+// 3step: git commit -m "package.json"
+
+// 2nd step: tsc --init
+// 2step: git add .
+// 3step: git commit -m "tsconfig.json"
+
+
+// Then create a file index.ts then adopt above steps:
+// git add . 
+// git commit -m "index.ts file added"
+
+
+
+
+console.log("Hello World!");
